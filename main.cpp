da6ba58c8a4963f577942dd79abedcf095c59a17
arnaud BLONDET <arnaud.blondet31400@gmail.com>
2024-12-06 10:43:53 +0100

    renaut first commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(){
 voiture peugeot;
 std::cout<<"kilometres:"<<peugeot.get_kilometres()<<std::endl;
+std::cout<<"couleur:"<<peugeot.get_couleur()<<std::endl;
 
 return 0;
 }
--- voiture.cpp
+++ voiture.cpp
@@ -1,11 +1,18 @@
 #include  "voiture.h"
+#include <string>
 
 voiture::voiture()
 {
     kilometres= 11500;
+    couleur="jaune";
 }
 
 int voiture::get_kilometres()
 {
     return kilometres;
+}
+
+std::string voiture::get_couleur()
+{
+    return couleur;
 }
